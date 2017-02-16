852d3b945f826622655fa09fb0ea042395f58417
deciassettesimo <deciassettesimo@gmail.com>
2017-02-16 18:04:44 +0300

    Parts 97

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,59 +2,34 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as coursesActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
+  // 1. Constructor - init state and bind functions
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      course: { title: '' }
-    };
-
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
   }
 
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course});
-  }
-
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course);
-  }
-
-  courseRow(course, index) {
-    return <div key={index}>{course.title}</div>;
-  }
-
+  // 3. Render function
   render() {
+    const {courses} = this.props;
+
     return (
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title}
-        />
-        <input
-          type="submit"
-          value="Save"
-          onClick={this.onClickSave}
-        />
+        <CourseList courses={courses} />
       </div>
     );
   }
 }
 
+// 4. Prop Types
 CoursesPage.propTypes = {
   actions: PropTypes.object.isRequired,
   courses: PropTypes.array.isRequired
 };
 
+// 5. Redux connect functions and export component
 function mapStateToProps(state, ownProps) {
   return {
     courses: state.courses
